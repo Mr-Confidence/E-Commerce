0477ecde55645b6fdc85bcae854fbcc09384e356
Mr-Confidence <145501133+Mr-Confidence@users.noreply.github.com>
2025-03-30 22:02:00 +0000

    E-Commerce Website

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import Container from "./ui/container";
+import Footer from "./ui/Footer";
+import Header from "./ui/Header";
+
+function App() {
+  return (
+    <main>
+      <Header />
+      <Container>
+        <p>Hello World</p>
+      </Container>
+      <Footer />
+    </main>
+  );
+}
+
+export default App;
---
+++ client/tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,jsx}"  // Add jsx to the file extensions
+  ],
+  theme: {
+    extend: {
+      colors: {
+        whiteText: "#fff",
+        darkText: "#000000",
+        lightText: "#9b9b9b",
+        greenText: "#1d8221",
+        redText: "#e02b2b",
+        skyText: "#32bde8",
+      },
+
+      flex: {full: "0 0 100%"},
+    }, 
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/aspect-ratio"),
+  ],
+};
